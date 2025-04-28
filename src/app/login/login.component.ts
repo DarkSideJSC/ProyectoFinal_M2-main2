bbdeeb980382b9308d27d66765b2374f50d0ad03
JavierSC <javiersolizc@gmail.com>
2025-04-27 20:04:47 -0400

    Listo para cargar el componente Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [ ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado', this.loginForm.value);
+    }
+  }
+
+}
